1b5112cdda3c8a9c2021f55e9261d90d2903fe49
Hamid <hamid@logicalclocks.com>
2025-06-26 16:05:45 +0200

    add comments for clarity

--- internal/transfer/utils.go
+++ internal/transfer/utils.go
@@ -6,6 +6,8 @@ package transfer
 import "C"
 import "unsafe"
 
+// Bypasses Go's environment variables cache and reads directly from OS.
+// Enables changing env variables at runtime (delta-rs is a use case)
 func getEnv(key string) string {
 	ck := C.CString(key)
 	defer C.free(unsafe.Pointer(ck))
